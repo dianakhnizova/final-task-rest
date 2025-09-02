444a5348b3d14f952a32d211064f4427abc5b0f6
Diana Khizhnova <khnizovad@gmail.com>
2025-09-02 17:07:54 +0500

    feat: add Button component for test

--- app/App.tsx
+++ app/App.tsx
@@ -1,7 +1,11 @@
+import { Button } from './components/ui/button/Button';
+import { messages } from './sources/messages';
+
 function App() {
   return (
     <div>
       <h1>Init project</h1>
+      <Button>{messages.buttons.testButton}</Button>
     </div>
   );
 }
