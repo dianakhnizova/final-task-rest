7e7a0327f13807c23b7f51d0de7b9390c574a1e1
Diana Khizhnova <khnizovad@gmail.com>
2025-09-02 16:58:17 +0500

    chore: test husky and fix stylelint

--- stylelint.config.cjs
+++ stylelint.config.cjs
@@ -1,21 +1,17 @@
 module.exports = {
   extends: [
-    'stylelint-config-standard-scss', 
-    'stylelint-config-prettier' 
+    "stylelint-config-standard",
+    "stylelint-prettier/recommended"
   ],
-  plugins: ['stylelint-scss'],
   rules: {
-    'color-hex-length': 'short',
-    'color-named': 'never',
-    'selector-class-pattern': '^[a-z][a-zA-Z0-9]*$',
-    'selector-id-pattern': '^[a-z][a-zA-Z0-9]*$',
-    'selector-pseudo-class-no-unknown': [
+    "color-hex-length": "short",
+    "color-named": "never",
+    "selector-class-pattern": "^[a-z][a-zA-Z0-9]*$",
+    "selector-id-pattern": "^[a-z][a-zA-Z0-9]*$",
+    "selector-pseudo-class-no-unknown": [
       true,
-      { ignorePseudoClasses: ['global'] }
-    ],
-
-    'at-rule-no-unknown': null,
-    'scss/at-rule-no-unknown': true
+      { ignorePseudoClasses: ["global"] }
+    ]
   },
-  ignoreFiles: ['**/node_modules/**', '**/dist/**']
+  ignoreFiles: ["**/node_modules/**", "**/dist/**"]
 };
