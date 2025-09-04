fc6860739240dabd93313e13297044dd0a98d7ec
Vavilov Alexander <oleksandr.vavilov@volvo.com>
2025-09-04 22:10:34 +1000

    feat: update button styles to use new theme

--- app/components/ui/button/Button.tsx
+++ app/components/ui/button/Button.tsx
@@ -1,14 +1,19 @@
 import classNames from 'classnames';
 import styles from './Button.module.css';
-import { memo } from 'react';
+import {
+  type ButtonHTMLAttributes,
+  type FC,
+  memo,
+  type ReactNode,
+} from 'react';
 import { Variant } from '../../../sources/enums';
 
 type Props = {
   variant?: Variant;
-  children?: React.ReactNode;
-} & React.ButtonHTMLAttributes<HTMLButtonElement>;
+  children?: ReactNode;
+} & ButtonHTMLAttributes<HTMLButtonElement>;
 
-export const Button: React.FC<Props> = memo(
+export const Button: FC<Props> = memo(
   ({ variant = Variant.PRIMARY, className, children, ...rest }: Props) => {
     return (
       <button
