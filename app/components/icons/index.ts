5fdb3ecfd6629b8e72ce8f713aa7f6f8bdc2890b
Vavilov Alexander <oleksandr.vavilov@volvo.com>
2025-09-08 01:28:55 +1000

    feat: add Sun and Language icons

--- app/components/icons/index.ts
+++ app/components/icons/index.ts
@@ -1,2 +1,4 @@
 export { GithubLogo } from './GithubLogo.tsx';
 export { RsSchoolLogo } from './RsSchoolLogo.tsx';
+export { LanguageIcon } from './LanguageIcon.tsx';
+export { SunIcon } from './SunIcon.tsx';
