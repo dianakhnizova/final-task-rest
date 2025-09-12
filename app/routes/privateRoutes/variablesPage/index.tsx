299cb899ba090bebc5fddca69baaa87b657b5980
Vavilov Alexander <oleksandr.vavilov@volvo.com>
2025-09-12 23:00:00 +1000

    feat: implement Variables page

--- app/routes/privateRoutes/variablesPage/index.tsx
+++ app/routes/privateRoutes/variablesPage/index.tsx
@@ -1,13 +1,22 @@
+import { useState } from 'react';
+
 import { variablesPage as messages } from '@/sources/messages/variablesPage.ts';
+
+import { type KeyValue, KeyValueEditor } from '@/components/keyValueEditor';
 
 import { pageMeta } from '@/utils/metaHelpers.ts';
 
 export const meta = pageMeta(messages);
 
 export default function VariablesPage() {
+  const [keyValues, setKeyValues] = useState<KeyValue[]>([]);
+
   return (
     <div>
       <h1>Variables</h1>
+      <div>
+        <KeyValueEditor keyValues={keyValues} onChange={setKeyValues} />
+      </div>
     </div>
   );
 }
