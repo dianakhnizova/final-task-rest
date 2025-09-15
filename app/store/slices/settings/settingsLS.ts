0f6fe21c956bf26acff726825a35e11d5bf1e582
Vavilov Alexander <oleksandr.vavilov@volvo.com>
2025-09-15 20:00:07 +1000

    refactor: extract `settingsLSKey` constant

--- app/store/slices/settings/settingsLS.ts
+++ app/store/slices/settings/settingsLS.ts
@@ -3,14 +3,16 @@ import {
   initialState,
 } from '@/store/slices/settings/settings.slice.ts';
 
+export const settingsLSKey = 'settings';
+
 export const saveSettingsToLS = (settings: SettingsState) => {
   const json = JSON.stringify(settings);
-  localStorage.setItem('settings', json);
+  localStorage.setItem(settingsLSKey, json);
 };
 
 export const loadSettingsFromLS = (): SettingsState => {
-  const json = localStorage.getItem('settings');
+  const json = localStorage.getItem(settingsLSKey);
   return json ? JSON.parse(json) : initialState;
 };
 
-export const clearSettingsLS = () => localStorage.removeItem('settings');
+export const clearSettingsLS = () => localStorage.removeItem(settingsLSKey);
