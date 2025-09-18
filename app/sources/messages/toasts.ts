89c791926b17b4ea62a4d1d5b54ef2b93e3f23db
Aleksandr Vavilov <alvi.ua@gmail.com>
2025-09-18 22:33:08 +0300

    refactor: change UI for input URL & add toast for init disconect for user

--- app/sources/messages/toasts.ts
+++ app/sources/messages/toasts.ts
@@ -8,7 +8,9 @@ export const toasts = {
   signUpProcessId: 'signUpProcessId',
   errorConfirmEmail:
     'Please confirm your email from your inbox before signing in.',
+  logOutInit: 'Disconnect a user from the system...',
   logOut: 'have been logged out.',
+  logOutId: 'logOutId',
   userExist: 'This email is already registered. Please try another one.',
   userExistId: 'userExistId',
   invalidPasswordEmail: 'Invalid password or e-mail',
