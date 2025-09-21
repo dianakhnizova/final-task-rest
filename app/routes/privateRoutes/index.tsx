071b12ddc0478ab8d20b6e14d3acce5d1027d60f
Diana Khizhnova <khnizovad@gmail.com>
2025-09-21 17:05:58 +0500

    fix: redirect after token expire

--- app/routes/privateRoutes/index.tsx
+++ app/routes/privateRoutes/index.tsx
@@ -1,19 +1,14 @@
-import {
-  selectAuth,
-  selectIsAuthenticated,
-} from '@/store/slices/auth/selectors';
-import { Suspense, useEffect } from 'react';
+import { selectIsAuthenticated } from '@/store/slices/auth/selectors';
+import { useEffect } from 'react';
 import toast from 'react-hot-toast';
 import { useSelector } from 'react-redux';
 import { Outlet, useNavigate } from 'react-router';
 import { AppRoutes, LS_KEY } from '@/sources/enums';
 import { toasts as toastMessages } from '@/sources/messages/toasts';
-import { WaitingLoader } from '@/components/ui/waitingLoader';
 import { useActions } from '@/utils/hooks/useActions';
 import { useSaveUserToLS } from '@/utils/hooks/useSaveUserToLS';
 
 export default function PrivateRoutes() {
-  const user = useSelector(selectAuth);
   const isTokenValid = useSelector(selectIsAuthenticated);
 
   const { setError, clearUser } = useActions();
@@ -21,8 +16,6 @@ export default function PrivateRoutes() {
   const navigate = useNavigate();
 
   useEffect(() => {
-    console.log({ user, isTokenValid });
-
     if (!isTokenValid) {
       clearUser();
       removeUserFromStorage();
@@ -35,9 +28,5 @@ export default function PrivateRoutes() {
     }
   }, [navigate, isTokenValid, setError]);
 
-  return (
-    <Suspense fallback={<WaitingLoader />}>
-      {user ? <Outlet /> : <WaitingLoader />}
-    </Suspense>
-  );
+  return <Outlet />;
 }
